bca955d1fc764d6bdf18c3ec3f59cf38b409b080
Anna Völkl <voelkl@e-conomix.at>
2020-02-06 09:06:45 +0100

    Set utf8-encoding for To address
    See https://github.com/magepal/magento2-gmail-smtp-app/issues/152#issuecomment-581414502

--- Model/TwoDotThree/Smtp.php
+++ Model/TwoDotThree/Smtp.php
@@ -80,6 +80,7 @@ class Smtp
 
         $encoding = $message->getEncoding();
         $message = Message::fromString($message->getRawMessage());
+        $message->getHeaders()->get('to')->setEncoding('utf-8');
         $message->getHeaders()->setEncoding('utf-8');
         $message->setEncoding($encoding);
 
