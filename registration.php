d6c6aacfc0c68d24cb7a1bb40cd619499f74d159
Renon Stewart <s.renon@gmail.com>
2020-04-29 15:24:21 -0400

    Code refactoring

--- registration.php
+++ registration.php
@@ -5,8 +5,10 @@
  * http://www.magepal.com | support@magepal.com
  */
 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
     'MagePal_GmailSmtpApp',
     __DIR__
 );
